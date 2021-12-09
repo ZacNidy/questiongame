74881bce1e3c41f43cfa9081d7b5cacab2e96e3a
ZacNidy <95726264+ZacNidy@users.noreply.github.com>
2021-12-08 22:40:23 -0500

    Add files via upload

---
+++ question_game.py
@@ -0,0 +1,26 @@
+# Welcome to the coolest question guessing game
+
+name = input('Name: ')
+play = input('Welcome {}, would you like to play? (y,n): '.format(name)).lower()
+
+score = 0
+
+if play != 'y'.lower():
+    print('Quitting...')
+    quit()
+
+print('Okay lets get it started')
+
+while True:
+    user_answer = input('What is the capital of the United States? ').lower()
+    if user_answer == 'united states':
+        print('That is correct!')
+        score += 1
+    else:
+        print('That is not correct')
+        restart = input('Would you like to restart? (y,n): ').lower()
+        if restart != 'y'.lower():
+            print('Quitting...')
+            break
+        else:
+            continue
